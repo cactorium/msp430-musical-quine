caa3c29d30d129f2ef6eb102f131edb5a46b2735
Kelvin Ly <kelvin.ly1618@gmail.com>
2017-04-02 10:52:37 -0400

    Add Python script

---
+++ fixed_point.py
@@ -0,0 +1,23 @@
+import subprocess
+import sys
+
+f = open("./test.c", "r")
+cur_file = bytes(f.read(), "utf8")
+f.close()
+
+p = subprocess.run(["target/debug/quine"],
+                   input=cur_file,
+                   stdout=subprocess.PIPE)
+new_file = p.stdout
+
+count = 0
+while cur_file != new_file:
+  print("count {}".format(count),file=sys.stderr)
+  cur_file = new_file
+  p = subprocess.run(["target/debug/quine"],
+                    input=cur_file,
+                    stdout=subprocess.PIPE)
+  new_file = p.stdout
+  count += 1
+
+print(str(cur_file))
